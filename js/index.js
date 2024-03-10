e1692da909155b19f59cc9bf4eaa0df1410bb3f8
Yulii <yulikkrasulia@gmail.com>
2024-03-10 09:49:40 +0200

    done with css

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const game = document.querySelector(".game");
+const res = document.querySelector(".res");
+const btnGame = document.querySelector(".new-game");
+const fields = document.querySelectorAll(".field");
+const step = false;
+const circle = "<circle r="100" cx="116" cy="116" stroke="blue" stroke-width="10" fill="none" stroke-linecup="round"/>";
+const cross = "<line class="first" x1="15" y1="15" x2="220" y2="220" stroke="red" stroke-width="10" stroke-linecup="round"/>< line class="second" x1 = "220" y1 = "15" x2 = "15" y2 = "220" stroke = "red" stroke - width="10" stroke - linecup="round" />"
