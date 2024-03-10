57556b6d619967ed68bf00ef79d84909966d5c04
Yulii <yulikkrasulia@gmail.com>
2024-03-10 09:58:44 +0200

    funcs created

--- js/index.js
+++ js/index.js
@@ -4,4 +4,27 @@ const btnGame = document.querySelector(".new-game");
 const fields = document.querySelectorAll(".field");
 const step = false;
 const circle = "<circle r="100" cx="116" cy="116" stroke="blue" stroke-width="10" fill="none" stroke-linecup="round"/>";
-const cross = "<line class="first" x1="15" y1="15" x2="220" y2="220" stroke="red" stroke-width="10" stroke-linecup="round"/>< line class="second" x1 = "220" y1 = "15" x2 = "15" y2 = "220" stroke = "red" stroke - width="10" stroke - linecup="round" />"
+const cross = "<line class="first" x1="15" y1="15" x2="220" y2="220" stroke="red" stroke-width="10" stroke-linecup="round"/>< line class="second" x1 = "220" y1 = "15" x2 = "15" y2 = "220" stroke = "red" stroke - width="10" stroke - linecup="round" />";
+
+function stepCross() {
+
+}
+
+function stepZero() {
+
+}
+
+function init() {
+
+}
+
+function newGame() {
+
+}
+
+function win() {
+
+}
+
+btnGame.addEventListener("click", newGame);
+game.addEventListener("click", init)
